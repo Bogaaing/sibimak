4b7c90c20c892f6bc4def892ac20fd93f5c24a0f
Achepp <ahmadasepsuhendi@gmail.com>
2026-09-08 11:00:12 +0700

    ramping

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -132,7 +132,7 @@ export const Login: React.FC = () => {
       <div className="relative z-10 w-full max-w-[1440px] mx-auto px-6 sm:px-12 lg:px-16 py-6 sm:py-8 flex-1 flex flex-col justify-between">
         {/* UPPER & MIDDLE WORKSPACE */}
         <div className="flex-1 flex flex-col items-center lg:items-start justify-center my-auto w-full">
-          <div className="w-full max-w-[390px] sm:max-w-[405px] space-y-4 lg:ml-4 xl:ml-8">
+          <div className="w-full max-w-[360px] space-y-4 lg:ml-4 xl:ml-8 mx-auto lg:mx-0">
             {/* BRANDING & WELCOME MESSAGE ABOVE FORM */}
             <div className="space-y-3">
               {/* Logo & SiBiMa Text */}
